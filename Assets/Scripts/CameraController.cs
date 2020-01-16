ac14e19d957bce4f1c3ddecdc6dd2e0205f95776
HUGO SEVERIN <severin.hugo@gmail.com>
2020-01-16 13:33:12 +0100

    1st

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField]
+    private float smoothSpeed;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void LateUpdate()
+    {
+        //MARKER Method 1
+        //transform.position = new Vector3(target.position.x, target.position.y, target.position.z);  //transform.position = camera.position
+
+        //MARKER Method 2
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, -10), smoothSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField]
+    private float movespeed = 5.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * movespeed;
+        moveV = Input.GetAxisRaw("Vertical") * movespeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
