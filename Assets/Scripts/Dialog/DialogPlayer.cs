a034e624dee7733c7398515af63d2c75fc0c1359
ArndGlh <arnaudguilhaumon@gmail.com>
2020-01-27 11:27:17 +0100

    dialogue avec pnj ajouté

---
+++ Assets/Scripts/Dialog/DialogPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private Dialog m_Dialog = null;
+
+    [SerializeField]
+    private DialogBox m_DialogBox = null;
+
+    [SerializeField]
+    public bool playerInRange;
+
+    private void PlayDialog() {
+        m_DialogBox.DisplayDialog(m_Dialog);
+    }
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange){
+            if(m_DialogBox.gameObject.activeInHierarchy){
+                            m_DialogBox.CloseDialog();
+                        }else  {
+                        PlayDialog();
+                        }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            playerInRange = false;
+            m_DialogBox.CloseDialog();
+        }
+    }
+}
